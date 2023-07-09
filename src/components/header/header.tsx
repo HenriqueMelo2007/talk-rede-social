649a1478d4182ee7c0d95a9926fbba1b29b82ede
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-07-09 20:18:43 -0300

    estilizando Header e adicionando fonte

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,25 +1,38 @@
 import { Link } from 'react-router-dom';
+import './header.css'
 
 export function Header() {
   return (
-    <header>
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/perfil'>Perfil</Link>
-        </li>
-        <li>
-          <Link to='/posts'>Posts</Link>
-        </li>
-        <li>
-          <Link to='/amigos'>Amigos</Link>
-        </li>
-        <li>
-          <Link to='/adicionaramizade'>Adicionar amizades</Link>
-        </li>
-      </ul>
+    <header className='header'>
+
+      <div className='boxLogo'>
+        <img className='imgLogo' src="https://cdn.discordapp.com/attachments/1126699315247722506/1127608165480018030/w-logo-rose.gif" alt="Logo Walk" />
+        <h1 className='tittleLogo'>Walk</h1>
+      </div>
+
+      <div className="entrarMenu">
+        <button className='btnEntrar'>Entrar</button>
+
+        <nav>
+          <ul className='listaHeader'>
+            <li>
+              <Link className='itemListaHeader' to='/'>Home</Link>
+            </li>
+            <li >
+              <Link className='itemListaHeader' to='/perfil'>Perfil</Link>
+            </li>
+            <li >
+              <Link className='itemListaHeader' to='/posts'>Posts</Link>
+            </li>
+            <li>
+              <Link className='itemListaHeader' to='/amigos'>Amigos</Link>
+            </li>
+            <li>
+              <Link className='itemListaHeader'  to='/adicionaramizade'>Adicionar amizades</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   )
 }
