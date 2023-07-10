bb807df9a07451e112571efd384702ee85713107
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-07-10 16:45:15 -0300

    construindo login

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,16 +1,29 @@
+import { Link } from "react-router-dom";
+import './login.css'
+
 export function Login () {
   return (
-    <div className="boxLogin">
-      <h2></h2>
-      <div className="boxLoginInputs">
-        <input type="text" />
-        <input type="password" />
+    <div className="containerLogin">
+      <div className="boxLogin">
+
+        <h2 className="loginTittle">Logar</h2>
+
+        <div className="boxLoginInputs">
+          <input placeholder="Usuário" className="inputLoginText inputLogin" type="text" />
+          <input placeholder="Senha" className="inputLoginPassword inputLogin" type="password" />
+          <span className="esqueceuSenha">Esqueceu sua senha?</span>
+        </div>
+
+        <div className="boxLoginBtn">
+          <Link to='/'>Entrar</Link>
+        </div>
+
+        <div className="boxLoginCadastro">
+          <p>Não possui conta?</p>
+          <span>Cadastre-se</span>
+        </div>
       </div>
-      <div className="boxLoginBtn"><button>Entrar</button></div>
-      <div className="boxLoginCadastro">
-        <p></p>
-        <span></span>
-      </div>
+
     </div>
   )
 }
