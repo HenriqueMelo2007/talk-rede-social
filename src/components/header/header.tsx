f54c24c0a3dac147baaaf6ac77ee908607b5d5cf
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-07-09 21:06:41 -0300

    estilizando header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,20 @@
 import { Link } from 'react-router-dom';
 import './header.css'
+import iconHamburguer from '../../assets/hamburguer-icon.png'
+import { useState } from 'react';
 
 export function Header() {
+
+  const [showBtnIcon, setShowBtnIcon] = useState(true)
+  const [showNav, setShowNav] = useState(false)
+
+
+  function toggleShow () {
+    setShowBtnIcon(!showBtnIcon)
+    setShowNav(!showNav)
+  }
+
+
   return (
     <header className='header'>
 
@@ -11,27 +24,36 @@ export function Header() {
       </div>
 
       <div className="entrarMenu">
-        <button className='btnEntrar'>Entrar</button>
 
+        {showBtnIcon && 
+        <div className='divEntrarHamburguer'>
+          <button className='btnEntrar'>Entrar</button>
+          <img onClick={toggleShow} className='iconHamburguer' src={iconHamburguer} alt="" />
+        </div>}
+
+        {showNav && 
         <nav>
-          <ul className='listaHeader'>
-            <li>
-              <Link className='itemListaHeader' to='/'>Home</Link>
-            </li>
-            <li >
-              <Link className='itemListaHeader' to='/perfil'>Perfil</Link>
-            </li>
-            <li >
-              <Link className='itemListaHeader' to='/posts'>Posts</Link>
-            </li>
-            <li>
-              <Link className='itemListaHeader' to='/amigos'>Amigos</Link>
-            </li>
-            <li>
-              <Link className='itemListaHeader'  to='/adicionaramizade'>Adicionar amizades</Link>
-            </li>
-          </ul>
-        </nav>
+        <ul className='listaHeader'>
+          <li>
+            <Link className='itemListaHeader' to='/'>Home</Link>
+          </li>
+          <li >
+            <Link className='itemListaHeader' to='/perfil'>Perfil</Link>
+          </li>
+          <li >
+            <Link className='itemListaHeader' to='/posts'>Posts</Link>
+          </li>
+          <li>
+            <Link className='itemListaHeader' to='/amigos'>Amigos</Link>
+          </li>
+          <li>
+            <Link className='itemListaHeader'  to='/adicionaramizade'>Adicionar amizades</Link>
+          </li>
+          <li onClick={toggleShow} id='fecharNav'>X</li>
+        </ul>
+      </nav>}
+
+        
       </div>
     </header>
   )
