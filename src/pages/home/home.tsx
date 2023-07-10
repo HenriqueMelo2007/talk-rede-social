b19fa7b872d3ddc35d8405b08fcb8fc526425e08
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-07-10 17:22:33 -0300

    Home page concluída

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,21 @@
+import './home.css'
+
 export function Home () {
   return (
-    <h1>Home page</h1>
+    <div className="postHome">
+
+      <header className='headerHome'>
+        <div className="fotoPerfil"></div>
+        <h2 className='nomeUsuario'>Usuário123</h2>
+      </header>
+
+      <textarea cols="30" rows="10"></textarea>
+
+      <div className="publicar">
+        <p>10/07/2023</p>
+        <button className='btnPublicar'>Publicar</button>
+      </div>
+
+    </div>
   )
 }
