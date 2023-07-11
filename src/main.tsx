702f79e73114b480c6ee8037dbc583f8241e8042
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-07-11 19:30:19 -0300

    modificando nome

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
 import './index.css'
-import { ProvedorMeuContexto } from './contexto/Contexto.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ProvedorMeuContexto>
-      <BrowserRouter>
-        <App /> 
-      </BrowserRouter>          
-    </ProvedorMeuContexto>    
+    <BrowserRouter>
+      <App /> 
+    </BrowserRouter>             
   </React.StrictMode>,
 )
